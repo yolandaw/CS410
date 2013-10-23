11a7c1c016ec61107163389f19129996e081227c
Lenox <Lenox@206.87.120.212>
2013-10-23 12:22:27 -0700

    JSFML Window Second Attempt

---
+++ CS410 Project/src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		RenderWindow window = new RenderWindow();
+		
+		VideoMode mode = new VideoMode(800, 600);
+		
+		window.create(mode, "Test");
+		
+		window.setFramerateLimit(60);
+		
+		boolean RUNNING = true;
+		
+		while (RUNNING) {
+			
+			for (Event event : window.pollEvents()) {
+				
+				if (event.type == Event.Type.CLOSED) {
+					RUNNING = false;
+				}
+			}
+			
+			window.clear();
+			window.display();
+		}
+		
+		window.close();
+
+	}
+
+}
