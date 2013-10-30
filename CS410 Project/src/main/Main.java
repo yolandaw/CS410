75b6fb00aa1b0238eccdea561b98ce0e228e7b9e
Lenox <lenoxchew@gmail.com>
2013-10-30 01:19:11 -0700

    removed hard coding of current directory when create log gatherer, added basic visualization (no comments yet)

--- CS410 Project/src/main/Main.java
+++ CS410 Project/src/main/Main.java
@@ -1,13 +1,16 @@
 package main;
 
 
+import java.io.File;
 import java.io.IOException;
+
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
-import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.*;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.event.Event;
+
 
 public class Main {
 
@@ -18,14 +21,17 @@ public class Main {
 	 * @throws NoHeadException 
 	 */
 	public static void main(String[] args) throws IOException, NoHeadException, GitAPIException {
+		
+		File currentDir = new File(System.getProperty("user.dir")).getParentFile();
+		String gitDir = currentDir + "/.git";
 		//quinn test start
-		LogGatherer lg = new LogGatherer("C:/Users/Quinn/Documents/Homework/CPSC 410/CS410/test/.git", "CS410 Project/src/main/Main.java");
+		LogGatherer lg = new LogGatherer(gitDir, "CS410 Project/src/main/Main.java");
 		String[] strArray = lg.rawCode();
 		for(int i=0; i<lg.numLinesOfCode(); i++){
 			System.out.print(lg.getAuthor(i).getName() + ": " + strArray[i]);
 		}
 		//quinn test end
-		
+
 		RenderWindow window = new RenderWindow();
 		
 		VideoMode mode = new VideoMode(800, 600);
@@ -34,6 +40,20 @@ public class Main {
 		
 		window.setFramerateLimit(60);
 		
+		Color backGroundColor = new Color (178,224,250);
+		
+		Floor floor1 = new Floor("foo");
+		Floor floor2 = new Floor("bar");
+		Floor floor3 = new Floor("boo");
+		Floor floor4 = new Floor("abc");
+		floor1.setFloorPosition(window.getSize().x/2, window.getSize().y); // should be set based on a buildings(class) position field?
+		floor1.splitFloorOwnership();
+		floor2.setFloorPosition(window.getSize().x/2, floor1.getFloorBoundaries().top-floor1.getFloorBoundaries().height);
+		floor2.splitFloorOwnership();
+		floor3.setFloorPosition(window.getSize().x/2, floor2.getFloorBoundaries().top-floor2.getFloorBoundaries().height);
+		floor3.splitFloorOwnership();
+		floor4.setFloorPosition(window.getSize().x/2, floor3.getFloorBoundaries().top-floor3.getFloorBoundaries().height);
+		floor4.splitFloorOwnership();
 		boolean RUNNING = true;
 
 		while (RUNNING) {
@@ -43,9 +63,14 @@ public class Main {
 				if (event.type == Event.Type.CLOSED) {
 					RUNNING = false;
 				}
+				
 			}
-			
-			window.clear();
+
+			window.clear(backGroundColor);
+			window.draw(floor1);
+			window.draw(floor2);
+			window.draw(floor3);
+			window.draw(floor4);
 			window.display();
 		}
 		
