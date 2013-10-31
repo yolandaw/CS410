4f2d259e95b6e503642fa0513c639cfe75650a83
Chiho <blackho8291@gmail.com>
2013-10-31 19:51:34 +0900

    Final version of the Parse class.
    I tested out for the basic case that has no nested class and it works
    well. 
    I wrote this to handle the case that has nested classes, but havent
    tested out for the nested class case yet.

--- CS410 Project/src/main/Parser.java
+++ CS410 Project/src/main/Parser.java
@@ -13,21 +13,18 @@ public class Parser {
 	private LogGatherer parsedClass;
 	private LinkedList<Class> createdClassObjects; 
 	private int currentLineNum; 
-	private Method currentMethod; 
-	private int blockCommentHandler; 
-	private int methodHandler; 
-	private int classHandler;
-	private boolean lineInMethod;
 	private String currentLine;
+	private int blockCommentHandler; 
+	private int classHandler;
+	private Method currentMethod; 
+	private int methodHandler; 
+	private boolean lineInMethod;
 	
 
-	public Parser(){
-		parsedClass = new LogGatherer();
-	}
+	public Parser(){}
 
-	
-	public void startParsingClass(String localRepoUrl, String parsingClass) throws IOException, GitAPIException {
-		parsedClass.startGatheringLog(localRepoUrl, parsingClass);
+	public void startParsingClass(LogGatherer gatheredLog) throws IOException, GitAPIException {
+		parsedClass = gatheredLog;
 		currentLineNum = 0;
 		createdClassObjects = new LinkedList<Class>();
 		blockCommentHandler = -1;
@@ -42,13 +39,9 @@ public class Parser {
 			currentLineNum = i;
 			currentLine = parsedClass.rawCode(i);
 			parsingCodeLine(currentLine);
-		//	System.out.println(currentLineNum);
-		//	System.out.println(classHandler);
-			System.out.print(lineInMethod + " ");
-			System.out.println(currentLineNum+1 + "   " + methodHandler);
 		}
 		
-		// test
+		// parsing test
 		int check = createdClassObjects.size();
 		System.out.println(check);
 		int a = 0;
@@ -65,11 +58,7 @@ public class Parser {
 				theMethod.printOwnerships();
 			}
 		    a++;
-		} // test end
-		
-		
-		// sends the createdClassObjects and createdMethodObjects to the visualization class
-		//...
+		} // parsing test end
 	}
 	
 	public void parsingCodeLine(String codeLine) throws GitAPIException, IOException {
@@ -87,13 +76,13 @@ public class Parser {
 				return;
 			}
 			// filter the code line that is outside a method and contains no actual codes such as comments: line comment, block comment, and documentation comment
-			else if(token.contains("//")) {
+			else if(currentLine.contains("*/") && blockCommentHandler == 1) {
+				blockCommentHandler = -1;
 				return;
 			}else if(token.contains("/*")) {
 				blockCommentHandler = 1;
 				return;
-			}else if(token.contains("*/")) {
-				blockCommentHandler = -1;
+			}else if(token.contains("//")) {  
 				return;
 			}else if(blockCommentHandler == 1) {
 				return;
@@ -104,102 +93,72 @@ public class Parser {
 				if(lineInMethod) {
 						
 					// filter the code line that is inside a method and contains no actual codes such as comment: line comment and block comment
-				//	if(token.contains("//")) {
-				//		return;
-				//	}else if(token.contains("/*")) {
-				//		blockCommentHandler = 1;
-				//		return;
-				//	}else if(token.contains("*/")) {
-				//		blockCommentHandler = -1;
-				//		return;
-				//	}else if(blockCommentHandler == 1) {
-				//		return;
-				//	}else { 
-				//		String lineCode = parsedClass.rawCode(currentLineNum);
-						
-						if(currentLine.contains("{") || currentLine.contains("}")) {
-								int result = 0;
+					if(currentLine.contains("{") || currentLine.contains("}")) {
+						int result = 0;
 							
-								// only check first most character and the last most character since it can be }String.contains("}"); {
-								// case 1: both '{' and '}' can be in the same line
-								if(currentLine.contains("{") && currentLine.contains("}")) {
-																
-									if(token.charAt(0) == '}') {
-										result--;
-									}else if(token.charAt(0) == '{') {
-										result++;
-									}
-									
-									while(tokenizer.hasMoreTokens()) {
-										token = tokenizer.nextToken();
-									}
-									int lastIndex = token.length() - 1;
+						// only check first most character and the last most character 
+						// case 1: both '{' and '}' can be in the same line
+						if(currentLine.contains("{") && currentLine.contains("}")) {
+							
+							if(token.charAt(0) == '}') {
+								result--;
+							}else if(token.charAt(0) == '{') {
+								result++;
+							}
+							
+							while(tokenizer.hasMoreTokens()) {
+								token = tokenizer.nextToken();
+							}
+							
+							int lastIndex = token.length() - 1;
+							if (token.charAt(lastIndex) == '}') {
+								result--;
+							}else if(token.charAt(lastIndex) == '{') {
+								result++;
+							}
 								
-									if (token.charAt(lastIndex) == '}') {
-										result--;
-									}else if(token.charAt(lastIndex) == '{') {
-										result++;
-									}
-								// case2: only one of '{' and '}' is in the line  
-								}else {
-									if(token.charAt(0) == '}') {
-										result--;
-									}else if (token.charAt(0) == '{') {
-										result++;
-									}else {
-										while(tokenizer.hasMoreTokens()) {
-											token = tokenizer.nextToken();
-										}
-										int lastIndex = token.length() - 1;
-										if(token.charAt(lastIndex) == '}') {
-											result--;
-										}else if(token.charAt(lastIndex) == '{') {
-											result++;
-										}
-									}
+						// case2: only one of '{' and '}' is in the line  
+						}else {
+							if(token.charAt(0) == '}') {
+								result--;
+							}else if (token.charAt(0) == '{') {
+								result++;
+							}else {
+								while(tokenizer.hasMoreTokens()) {
+									token = tokenizer.nextToken();
 								}
-								
-								methodHandler += result;
-								
-								if(methodHandler == -1) {
-									lineInMethod = false;	
-									PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
-									currentMethod.increOwnershipSize(ownership);
-									createdClassObjects.get(classHandler).addMethod(currentMethod);
-									return;
+								int lastIndex = token.length() - 1;
+								if(token.charAt(lastIndex) == '}') {
+									result--;
+								}else if(token.charAt(lastIndex) == '{') {
+									result++;
 								}
-						}
-							
-							
-						
-				/*		}else if(currentLine.contains("{")) {
-							methodHandler++;
-						}else if (currentLine.contains("}")) {
-							methodHandler--;
-							if(methodHandler == -1) {
-								lineInMethod = false;	
-								PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
-								currentMethod.increOwnershipSize(ownership);
-								createdClassObjects.get(classHandler).addMethod(currentMethod);
-								return;
 							}
 						}
-
-						PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
-						currentMethod.increOwnershipSize(ownership);
-					//	System.out.println(ownership.getName() + currentLineNum);
-				//	}	*/
 						
-								PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
-								currentMethod.increOwnershipSize(ownership);
-						
+						methodHandler += result;
+								
+						if(methodHandler == -1) {
+							lineInMethod = false;	
+							
+							PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
+							Author author = new Author(ownership.getName(), ownership.getEmailAddress());
+							currentMethod.increOwnershipSize(author);
+							
+							createdClassObjects.get(classHandler).addMethod(currentMethod);
+							return;
+						}
+					}
+					PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
+					Author author = new Author(ownership.getName(), ownership.getEmailAddress());
+					currentMethod.increOwnershipSize(author);	
 								
 				}else if(classHandler > -1 && currentLine.contains("}")) {
 					classHandler--;
 			
 				}else {
 									
-					if((token.equals("public") || token.equals("private") || token.equals("protected"))) {
+					if(token.equals("public") || token.equals("private") || token.equals("protected")) {
 						token = tokenizer.nextToken();
 					}
 					
@@ -238,7 +197,7 @@ public class Parser {
 						classCreator(className);
 					}else {
 						// the line will contain one of these: method contents, variable name
-						// could be the constructor
+						// the method could be the constructor
 						if(token.contains("(")) {
 							StringTokenizer tokenizer2 = new StringTokenizer(token, "(");
 							String methodName = tokenizer2.nextToken();
@@ -282,10 +241,15 @@ public class Parser {
 		if(currentLine.contains("{")) {
 			methodHandler++;	
 		}
-		System.out.println("Method is created");
 		currentMethod= new Method(methodName);
-		PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
-		currentMethod.adjustOwnership(ownership, 1);
 		lineInMethod = true;
+		
+		PersonIdent ownership = parsedClass.getAuthor(currentLineNum);
+		Author author = new Author(ownership.getName(), ownership.getEmailAddress());
+		currentMethod.adjustOwnership(author, 1);
+	}
+	
+	public LinkedList<Class> getParsedLog() {
+		return createdClassObjects;	
 	}
 }
