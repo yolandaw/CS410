53c251189f514c989a592cfd524713e8776900da
Chiho <blackho8291@gmail.com>
2013-10-31 18:57:23 +0900

    Basic methods and fields are made for testing parser

--- CS410 Project/src/main/Author.java
+++ CS410 Project/src/main/Author.java
@@ -2,8 +2,20 @@ package main;
 
 public class Author {
 	
-
+	private String authorName;
+	private String emailAddress;
 	
-
+	public Author(String name, String emailaddr) {
+		authorName = name;
+		emailAddress = emailaddr;
+	}
+	
+	public String getAuthorName() {
+		return authorName; 
+	}
+	
+	public String getEmailAddr() {
+		return emailAddress;
+	}
 }
 
