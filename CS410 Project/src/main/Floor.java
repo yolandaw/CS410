e4cb0912b2082d38b7d2f62a0b306c222265adc1
Quinn <quinnhsu@hotmail.com>
2013-11-15 12:45:32 -0800

    merge abc

--- CS410 Project/src/main/Floor.java
+++ CS410 Project/src/main/Floor.java
@@ -16,203 +16,213 @@ import org.jsfml.system.Vector2f;
 
 public class Floor implements org.jsfml.graphics.Drawable {
 
-	private String floorName;
-	private VertexArray floorVertices;
-	private int numOfLines;
-	private Map<Author, Integer> ownerships = new HashMap<Author, Integer>(); 
-	private IntRect floorBoundaries;
-	private int depth;
-	private Texture texture;
-	private IntRect tilePosition;
-	
-	
-	public Floor(String functionName) {
-		setFloorName(functionName);
-		floorBoundaries = new IntRect(0, 0, 0, 0);
-		setFloorDimensions(100, 30);
-		floorVertices = new VertexArray(PrimitiveType.QUADS);
-		setDepth(15);
-		texture = new Texture();
-		texture.setRepeated(true);
-		tilePosition = new IntRect(0,0,0,0);
-	}
-	
-	public void setFloorBoundaries(int left, int top, int width, int height) {
-		floorBoundaries = new IntRect(left, top, width, height);
-	}
-	
-	public void setFloorPosition(int left, int top) {
-		floorBoundaries = new IntRect(left, top, floorBoundaries.width, floorBoundaries.height);
-	}
-	
-	public void setFloorDimensions(int width, int height) {
-		floorBoundaries = new IntRect(floorBoundaries.left, floorBoundaries.top, width, height);
-	}
-	
-	public IntRect getFloorBoundaries() {
-		return floorBoundaries;
-	}
-	
-	public void getNumOfOwners() {
-		ownerships.size();
-	}
-	
-	public int getNumOfLines() {
-		return numOfLines;
-	}
-	
-	public void updateNumOfLines() {
-		for (Map.Entry<Author, Integer> entry: ownerships.entrySet()) {
-			numOfLines = numOfLines + entry.getValue();
-		}
-	}
-	
-	public String getFloorName() {
-		return floorName;
-	}
-	
-	private void setFloorName(String functionName) {
-		floorName = functionName;
-	}
-	
-	private void setDepth(int newDepth) {
-		depth = newDepth;
-	}
-	
-	public void adjustOwnership(Author author, int size) {
-		ownerships.put(author, size);
-	}
-	
-	public void setTexture(Path path, IntRect position) {
-		try {
-			texture.loadFromFile(path,position);
-			setTilePosition(position);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void setTilePosition(IntRect position) {
-		tilePosition = position;
-	}
-	
-	public void increOwnershipSize(Author author) {	
-		Iterator<Author> itr = ownerships.keySet().iterator();
-		Author existAuthor;
-	
-		
-		while(itr.hasNext()) {
-			existAuthor = (Author) itr.next();
-			if(existAuthor.getEmailAddr().equals(author.getEmailAddr())) {
-				ownerships.put(existAuthor, ownerships.get(existAuthor) + 1);
-				return;
-			}
-		}
-		adjustOwnership(author, 1);
-	}
-	
-	public void splitFloorOwnership() {
-		floorVertices.clear();
-		
-		float tempX = floorBoundaries.left;
-		Color authorColor = new Color(0,0,0);
-		Vector2f topLeft = new Vector2f(0, 0);
-		Vector2f topRight = new Vector2f(0, 0);
-		Vector2f bottomRight = new Vector2f(0, 0);
-		Vector2f bottomLeft = new Vector2f(0, 0);
-		Vector2f textCoord = calculateTextureCoords();
-		
-		for (Map.Entry<Author, Integer> entry: ownerships.entrySet()) {
-			float percentOwnership = (float) entry.getValue()/numOfLines;
-			authorColor = entry.getKey().getAuthorColor();
-			
-			if ((int) tempX+(floorBoundaries.width*percentOwnership) > floorBoundaries.left+floorBoundaries.width) {
-				break;
-			}
-			
-			// texture coordinates
-			topLeft = new Vector2f(0, 0);
-			topRight = new Vector2f(textCoord.x*percentOwnership, 0);
-			bottomRight = new Vector2f(textCoord.x*percentOwnership, textCoord.y);
-			bottomLeft = new Vector2f(0, textCoord.y);
+        private String floorName;
+        private VertexArray floorVertices;
+        private int numOfLines;
+        private Map<Author, Integer> ownerships = new HashMap<Author, Integer>(); 
+        private IntRect floorBoundaries;
+        private int depth;
+        private Texture texture;
+        private IntRect tilePosition;
+        // 0 = public, 1 = private, 2 = protected
+        private int accessModifierType;
+        
+        
+        public Floor(String functionName) {
+                setFloorName(functionName);
+                floorBoundaries = new IntRect(0, 0, 0, 0);
+                setFloorDimensions(100, 30);
+                floorVertices = new VertexArray(PrimitiveType.QUADS);
+                setDepth(15);
+                texture = new Texture();
+                texture.setRepeated(true);
+                tilePosition = new IntRect(0,0,0,0);
+        }
+        
+        public void setFloorBoundaries(int left, int top, int width, int height) {
+                floorBoundaries = new IntRect(left, top, width, height);
+        }
+        
+        public void setFloorPosition(int left, int top) {
+                floorBoundaries = new IntRect(left, top, floorBoundaries.width, floorBoundaries.height);
+        }
+        
+        public void setFloorDimensions(int width, int height) {
+                floorBoundaries = new IntRect(floorBoundaries.left, floorBoundaries.top, width, height);
+        }
+        
+        public IntRect getFloorBoundaries() {
+                return floorBoundaries;
+        }
+        
+        public void getNumOfOwners() {
+                ownerships.size();
+        }
+        
+        public int getNumOfLines() {
+                return numOfLines;
+        }
+        
+        public void updateNumOfLines() {
+                for (Map.Entry<Author, Integer> entry: ownerships.entrySet()) {
+                        numOfLines = numOfLines + entry.getValue();
+                }
+        }
+        
+        public String getFloorName() {
+                return floorName;
+        }
+        
+        private void setFloorName(String functionName) {
+                floorName = functionName;
+        }
+        
+        private void setDepth(int newDepth) {
+                depth = newDepth;
+        }
+        
+        public void adjustOwnership(Author author, int size) {
+                ownerships.put(author, size);
+        }
+        
+        public void setTexture(Path path, IntRect position) {
+                try {
+                        texture.loadFromFile(path,position);
+                        setTilePosition(position);
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+        }
+        
+        private void setTilePosition(IntRect position) {
+                tilePosition = position;
+        }
+        
+        public void increOwnershipSize(Author author) {        
+                Iterator<Author> itr = ownerships.keySet().iterator();
+                Author existAuthor;
+        
+                
+                while(itr.hasNext()) {
+                        existAuthor = (Author) itr.next();
+                        if(existAuthor.getEmailAddr().equals(author.getEmailAddr())) {
+                                ownerships.put(existAuthor, ownerships.get(existAuthor) + 1);
+                                return;
+                        }
+                }
+                adjustOwnership(author, 1);
+        }
+        
+        public void splitFloorOwnership() {
+                floorVertices.clear();
+                
+                float tempX = floorBoundaries.left;
+                Color authorColor = new Color(0,0,0);
+                Vector2f topLeft = new Vector2f(0, 0);
+                Vector2f topRight = new Vector2f(0, 0);
+                Vector2f bottomRight = new Vector2f(0, 0);
+                Vector2f bottomLeft = new Vector2f(0, 0);
+                Vector2f textCoord = calculateTextureCoords();
+                
+                for (Map.Entry<Author, Integer> entry: ownerships.entrySet()) {
+                        float percentOwnership = (float) entry.getValue()/numOfLines;
+                        authorColor = entry.getKey().getAuthorColor();
+                        
+                        if ((int) tempX+(floorBoundaries.width*percentOwnership) > floorBoundaries.left+floorBoundaries.width) {
+                                break;
+                        }
+                        
+                        // texture coordinates
+                        topLeft = new Vector2f(0, 0);
+                        topRight = new Vector2f(textCoord.x*percentOwnership, 0);
+                        bottomRight = new Vector2f(textCoord.x*percentOwnership, textCoord.y);
+                        bottomLeft = new Vector2f(0, textCoord.y);
 
-			floorVertices.add(new Vertex(new Vector2f(tempX, floorBoundaries.top),authorColor,bottomLeft));
-			floorVertices.add(new Vertex(new Vector2f(tempX, floorBoundaries.top-floorBoundaries.height),authorColor,topLeft));
-			floorVertices.add(new Vertex(new Vector2f((float) (tempX+(floorBoundaries.width*percentOwnership)), floorBoundaries.top-floorBoundaries.height),authorColor,topRight));
-			floorVertices.add(new Vertex(new Vector2f((float) (tempX+(floorBoundaries.width*percentOwnership)), floorBoundaries.top),authorColor,bottomRight));
-			
-			// top of floor
-			addTopOfFloor(authorColor, tempX, percentOwnership);
-			
-			tempX = (float) (tempX+(floorBoundaries.width*percentOwnership));
-		}
-		
-		// side of floor
-		addSideOfFloor(authorColor, tempX);
-	}
-	
-	private void addTopOfFloor(Color authorColor, float leftSideX, float widthPercent) {
-		float lighten = (float) 1.3;
-		Color lighterColor = new Color((int) (authorColor.r * lighten),(int) (authorColor.g * lighten),(int) (authorColor.b * lighten), 255);
-		
-		Vector2f textCoord = calculateTextureCoords();
-		
-		Vector2f topLeft = new Vector2f(0, 0);
-		Vector2f topRight = new Vector2f(textCoord.x*widthPercent, 0);
-		Vector2f bottomRight = new Vector2f(textCoord.x*widthPercent, depth);
-		Vector2f bottomLeft = new Vector2f(0, depth);
-		
-		floorVertices.add(new Vertex(new Vector2f(leftSideX, floorBoundaries.top - floorBoundaries.height),lighterColor,bottomLeft));
-		floorVertices.add(new Vertex(new Vector2f(leftSideX + depth, floorBoundaries.top - floorBoundaries.height - depth),lighterColor,topLeft));
-		floorVertices.add(new Vertex(new Vector2f((float) (leftSideX+(floorBoundaries.width*widthPercent)) + depth, floorBoundaries.top-floorBoundaries.height - depth),lighterColor,topRight));
-		floorVertices.add(new Vertex(new Vector2f((float) (leftSideX+(floorBoundaries.width*widthPercent)), floorBoundaries.top - floorBoundaries.height),lighterColor,bottomRight));
-	}
-	
-	private void addSideOfFloor(Color authorColor, float rightSideX) {
-		float darken = (float) 0.8;
-		Color darkerColor = new Color((int) (authorColor.r * darken),(int) (authorColor.g * darken),(int) (authorColor.b * darken), 255);
-		
-		Vector2f textCoord = calculateTextureCoords();
-		
-		Vector2f topLeft = new Vector2f(0, 0);
-		Vector2f topRight = new Vector2f(depth, 0);
-		Vector2f bottomRight = new Vector2f(depth, textCoord.y);
-		Vector2f bottomLeft = new Vector2f(0, textCoord.y);
-		
-		floorVertices.add(new Vertex(new Vector2f(rightSideX, floorBoundaries.top),darkerColor,bottomLeft));
-		floorVertices.add(new Vertex(new Vector2f(rightSideX, floorBoundaries.top - floorBoundaries.height),darkerColor,topLeft));
-		floorVertices.add(new Vertex(new Vector2f(rightSideX + depth, floorBoundaries.top - floorBoundaries.height - depth),darkerColor,topRight));
-		floorVertices.add(new Vertex(new Vector2f(rightSideX + depth, floorBoundaries.top - depth),darkerColor,bottomRight));
-	}
-	
-	private Vector2f calculateTextureCoords() {
-		float xMapping = 0;
-		float yMapping = 0;
-		
-		if (tilePosition.width != 0 && tilePosition.height != 0) {
-			xMapping = Math.round(floorBoundaries.width/tilePosition.width) * tilePosition.width;
-			yMapping = Math.round(floorBoundaries.height/tilePosition.height) * tilePosition.height;
-		}
-		
-		return new Vector2f(xMapping, yMapping);
-	}
-	
-	// for parsing test
-	public void printOwnerships() {
-		Set<Author> authorSet = ownerships.keySet();
-		
-		Iterator<Author> authorIter = authorSet.iterator();
-		while(authorIter.hasNext()) {
-			Author author = authorIter.next();
-			System.out.println("Author Name: " + author.getAuthorName() + ", Ownership size: " + ownerships.get(author) + ".");
-		}
-	}
-	
-	
-	public void draw(RenderTarget target, RenderStates states) {
-		RenderStates state = new RenderStates(states, texture);
-		target.draw(floorVertices,state);
-	}
-	
+                        floorVertices.add(new Vertex(new Vector2f(tempX, floorBoundaries.top),authorColor,bottomLeft));
+                        floorVertices.add(new Vertex(new Vector2f(tempX, floorBoundaries.top-floorBoundaries.height),authorColor,topLeft));
+                        floorVertices.add(new Vertex(new Vector2f((float) (tempX+(floorBoundaries.width*percentOwnership)), floorBoundaries.top-floorBoundaries.height),authorColor,topRight));
+                        floorVertices.add(new Vertex(new Vector2f((float) (tempX+(floorBoundaries.width*percentOwnership)), floorBoundaries.top),authorColor,bottomRight));
+                        
+                        // top of floor
+                        addTopOfFloor(authorColor, tempX, percentOwnership);
+                        
+                        tempX = (float) (tempX+(floorBoundaries.width*percentOwnership));
+                }
+                
+                // side of floor
+                addSideOfFloor(authorColor, tempX);
+        }
+        
+        private void addTopOfFloor(Color authorColor, float leftSideX, float widthPercent) {
+                float lighten = (float) 1.3;
+                Color lighterColor = new Color((int) (authorColor.r * lighten),(int) (authorColor.g * lighten),(int) (authorColor.b * lighten), 255);
+                
+                Vector2f textCoord = calculateTextureCoords();
+                
+                Vector2f topLeft = new Vector2f(0, 0);
+                Vector2f topRight = new Vector2f(textCoord.x*widthPercent, 0);
+                Vector2f bottomRight = new Vector2f(textCoord.x*widthPercent, depth);
+                Vector2f bottomLeft = new Vector2f(0, depth);
+                
+                floorVertices.add(new Vertex(new Vector2f(leftSideX, floorBoundaries.top - floorBoundaries.height),lighterColor,bottomLeft));
+                floorVertices.add(new Vertex(new Vector2f(leftSideX + depth, floorBoundaries.top - floorBoundaries.height - depth),lighterColor,topLeft));
+                floorVertices.add(new Vertex(new Vector2f((float) (leftSideX+(floorBoundaries.width*widthPercent)) + depth, floorBoundaries.top-floorBoundaries.height - depth),lighterColor,topRight));
+                floorVertices.add(new Vertex(new Vector2f((float) (leftSideX+(floorBoundaries.width*widthPercent)), floorBoundaries.top - floorBoundaries.height),lighterColor,bottomRight));
+        }
+        
+        private void addSideOfFloor(Color authorColor, float rightSideX) {
+                float darken = (float) 0.8;
+                Color darkerColor = new Color((int) (authorColor.r * darken),(int) (authorColor.g * darken),(int) (authorColor.b * darken), 255);
+                
+                Vector2f textCoord = calculateTextureCoords();
+                
+                Vector2f topLeft = new Vector2f(0, 0);
+                Vector2f topRight = new Vector2f(depth, 0);
+                Vector2f bottomRight = new Vector2f(depth, textCoord.y);
+                Vector2f bottomLeft = new Vector2f(0, textCoord.y);
+                
+                floorVertices.add(new Vertex(new Vector2f(rightSideX, floorBoundaries.top),darkerColor,bottomLeft));
+                floorVertices.add(new Vertex(new Vector2f(rightSideX, floorBoundaries.top - floorBoundaries.height),darkerColor,topLeft));
+                floorVertices.add(new Vertex(new Vector2f(rightSideX + depth, floorBoundaries.top - floorBoundaries.height - depth),darkerColor,topRight));
+                floorVertices.add(new Vertex(new Vector2f(rightSideX + depth, floorBoundaries.top - depth),darkerColor,bottomRight));
+        }
+        
+        private Vector2f calculateTextureCoords() {
+                float xMapping = 0;
+                float yMapping = 0;
+                
+                if (tilePosition.width != 0 && tilePosition.height != 0) {
+                        xMapping = Math.round(floorBoundaries.width/tilePosition.width) * tilePosition.width;
+                        yMapping = Math.round(floorBoundaries.height/tilePosition.height) * tilePosition.height;
+                }
+                
+                return new Vector2f(xMapping, yMapping);
+        }
+        
+        // for parsing test
+        public void printOwnerships() {
+                Set<Author> authorSet = ownerships.keySet();
+                
+                Iterator<Author> authorIter = authorSet.iterator();
+                while(authorIter.hasNext()) {
+                        Author author = authorIter.next();
+                        System.out.println("Author Name: " + author.getAuthorName() + ", Ownership size: " + ownerships.get(author) + ".");
+                }
+        }
+        
+        
+        public void draw(RenderTarget target, RenderStates states) {
+                RenderStates state = new RenderStates(states, texture);
+                target.draw(floorVertices,state);
+        }
+        
+        public void setAccessType(int accessType) {
+                accessModifierType = accessType;
+        }
+        
+        public int getAccessType() {
+                return accessModifierType;
+        }
+        
 
 }
--- CS410 Project/src/main/Tower.java
+++ CS410 Project/src/main/Tower.java
@@ -9,102 +9,108 @@ import org.jsfml.graphics.RenderStates;
 import org.jsfml.graphics.RenderTarget;
 
 
-
 //Builds the class tower with name and information of number of floors and number of contributors
 //calls the Floor class functions to build internal floors?
 
 
 public class Tower implements org.jsfml.graphics.Drawable {
-	
-	private String towerName;
-	private int towerHeight;
-	private int towerWidth;
-	private int numberOfFloors;
-	private int floorHeight;
-	String[] arrayOfContributors;
-	private LinkedList<Floor> towerFloors = new LinkedList<Floor>();
-	
+        
+        private String towerName;
+        private int towerHeight;
+        private int towerWidth;
+        private int numberOfFloors;
+        private int floorHeight;
+        String[] arrayOfContributors;
+        private LinkedList<Floor> towerFloors = new LinkedList<Floor>();
+        private String cityName;
 
-	//constructing a new Tower/Class
-	public Tower(String className) {
-		setTowerName(className);
-		arrayOfContributors = new String[20];
-	}
-	
-	//goes through the lists of existing contributors and adds if is a new contributor 
-	public void contributorMovingIn(String authorName){
-		int i = 0;
-		
-		for(i = 0; i <= arrayOfContributors.length;){
-			if(!arrayOfContributors[i].equals(authorName))
-			{
-				i++;
-			}
-			else {
-				break;
-			}
-		}
-		arrayOfContributors[i] = authorName;
-		
-	}
-	
+        //constructing a new Tower/Class
+        public Tower(String className) {
+                setTowerName(className);
+                arrayOfContributors = new String[20];
+        }
+        
+        //goes through the lists of existing contributors and adds if is a new contributor 
+        public void contributorMovingIn(String authorName){
+                int i = 0;
+                
+                for(i = 0; i <= arrayOfContributors.length;){
+                        if(!arrayOfContributors[i].equals(authorName))
+                        {
+                                i++;
+                        }
+                        else {
+                                break;
+                        }
+                }
+                arrayOfContributors[i] = authorName;
+                
+        }
+        
 
-	//function that takes location x and y to build from city builder
-	
-	//sets the Tower Name to be drawn
-	public void setTowerName(String className){
-		towerName = className; 		
-	}
-	
-	//sets the standard floor height of each floor in tower
-	public int generateFloorHeight(){
-		Random rn = new Random();
-		floorHeight = rn.nextInt(5);
-		return floorHeight;
-	}
-	
-	//increases the number of floors in the tower
-	public void floorCounter(){
-		numberOfFloors++;
-	}
-	
+        //function that takes location x and y to build from city builder
+        
+        //sets the Tower Name to be drawn
+        public void setTowerName(String className){
+                towerName = className;                 
+        }
+        
+        //sets the standard floor height of each floor in tower
+        public int generateFloorHeight(){
+                Random rn = new Random();
+                floorHeight = rn.nextInt(5);
+                return floorHeight;
+        }
+        
+        //increases the number of floors in the tower
+        public void floorCounter(){
+                numberOfFloors++;
+        }
+        
     public void addFloor(Floor methodName) {
-    	towerFloors.add(methodName);
+            towerFloors.add(methodName);
     }
     
     public LinkedList<Floor> getListOfFloor() {
-    	return towerFloors;
+            return towerFloors;
     }
     
     public String getTowerName() {
-    	return towerName;
+            return towerName;
     }
     
-    // can add other floor updates in here
+ // can add other floor updates in here
     public void updateFloors() {
-    	
-    	int i = 0;
-    	towerFloors.get(0).setFloorPosition(400, 600);
-    	Floor previousFloor = null;
-    	for (Floor f: towerFloors) {
-    		f.updateNumOfLines();
-    		
-    		if (previousFloor != null) {
-    			f.setFloorPosition(400, previousFloor.getFloorBoundaries().top-previousFloor.getFloorBoundaries().height);
-    		}
-    		
-    		f.setTexture(FileSystems.getDefault().getPath("resources","texture.png"),new IntRect(0, 0, 32, 24));
-    		f.splitFloorOwnership();
-    		previousFloor = f;
-    		i++;
-    	}
+            
+            int i = 0;
+            towerFloors.get(0).setFloorPosition(400, 600);
+            Floor previousFloor = null;
+            for (Floor f: towerFloors) {
+                    f.updateNumOfLines();
+                    
+                    if (previousFloor != null) {
+                            f.setFloorPosition(400, previousFloor.getFloorBoundaries().top-previousFloor.getFloorBoundaries().height);
+                    }
+                    
+                    f.setTexture(FileSystems.getDefault().getPath("resources","texture.png"),new IntRect(0, 0, 32, 24));
+                    f.splitFloorOwnership();
+                    previousFloor = f;
+                    i++;
+            }
     }
 
-	@Override
-	public void draw(RenderTarget window, RenderStates renderStates) {
-		for (Floor f: towerFloors) {
-			window.draw(f);
-		}
-	}
-	
+        
+        public void draw(RenderTarget window, RenderStates renderStates) {
+                for (Floor f: towerFloors) {
+                        window.draw(f);
+                }
+        }
+    
+    public void setCityName(String packageName) {
+            cityName = packageName;
+    }
+    
+    public String getCityName() {
+            return cityName;
+    }
 }
