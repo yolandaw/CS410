82a2a24d16f876843bb04a6022d7fe11b87ef1e9
Quinn <quinnhsu@hotmail.com>
2013-11-15 12:47:11 -0800

    merge all

--- CS410 Project/src/main/Main.java
+++ CS410 Project/src/main/Main.java
@@ -20,117 +20,117 @@ import org.jsfml.window.event.MouseEvent;
 
 
 public class Main {
-	
-	private static String localRepoUrl = "";
-	public static int nameCount = 0;
-		
-/*	private static void findJavaClass(File file, String[] fileNames){
-		for(File temp : file.listFiles()){
-			if (temp.isFile()){
-				fileNames[nameCount] = temp.getName().toLowerCase();
-				nameCount++;
-//				}
-			}
-			if(temp.isDirectory()){
-				findJavaClass(temp, fileNames);
-			}
-		}
-			
-	}*/
-	
-	
+        
+        private static String localRepoUrl = "";
+        public static int nameCount = 0;
+                
+/*        private static void findJavaClass(File file, String[] fileNames){
+                for(File temp : file.listFiles()){
+                        if (temp.isFile()){
+                                fileNames[nameCount] = temp.getName().toLowerCase();
+                                nameCount++;
+//                                }
+                        }
+                        if(temp.isDirectory()){
+                                findJavaClass(temp, fileNames);
+                        }
+                }
+                        
+        }*/
+        
+        
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws GitAPIException 
-	 * @throws NoHeadException 
-	 */
-	public static void main(String[] args) throws IOException, NoHeadException, GitAPIException {
-		
-		//file chooser pop-up
-		JFileChooser chooser = new JFileChooser();
-		
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		chooser.setCurrentDirectory(new java.io.File("."));
-		chooser.setDialogTitle("Please select your GitHub repository folder. ");
-	    chooser.setAcceptAllFileFilterUsed(false);
-		int choice = chooser.showOpenDialog(null);
-				if(choice != JFileChooser.APPROVE_OPTION)
-					return;
-		File chooseFile = chooser.getSelectedFile();
-		
-		String filePath = chooseFile.getAbsolutePath();
-		System.out.println("filePath: " + filePath);
-		
-		//look for git meta data folder
-		File repoPath = new File(filePath);
-		String gitPath = null;
-		File[] mainDirFolders = null;
-		
-		if(repoPath.isDirectory()){
-			mainDirFolders = repoPath.listFiles();
-			for(int i=0;i<mainDirFolders.length;i++){
-				System.out.println("files in mainDir :" + mainDirFolders[i].getPath());
-				if(mainDirFolders[i].getPath().endsWith(".git")){
-				gitPath = mainDirFolders[i].getAbsolutePath();
+        /**
+         * @param args
+         * @throws IOException 
+         * @throws GitAPIException 
+         * @throws NoHeadException 
+         */
+        public static void main(String[] args) throws IOException, NoHeadException, GitAPIException {
+                
+                //file chooser pop-up
+                JFileChooser chooser = new JFileChooser();
+                
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                chooser.setCurrentDirectory(new java.io.File("."));
+                chooser.setDialogTitle("Please select your GitHub repository folder. ");
+            chooser.setAcceptAllFileFilterUsed(false);
+                int choice = chooser.showOpenDialog(null);
+                                if(choice != JFileChooser.APPROVE_OPTION)
+                                        return;
+                File chooseFile = chooser.getSelectedFile();
+                
+                String filePath = chooseFile.getAbsolutePath();
+                System.out.println("filePath: " + filePath);
+                
+                //look for git meta data folder
+                File repoPath = new File(filePath);
+                String gitPath = null;
+                File[] mainDirFolders = null;
+                
+                if(repoPath.isDirectory()){
+                        mainDirFolders = repoPath.listFiles();
+                        for(int i=0;i<mainDirFolders.length;i++){
+                                System.out.println("files in mainDir :" + mainDirFolders[i].getPath());
+                                if(mainDirFolders[i].getPath().endsWith(".git")){
+                                gitPath = mainDirFolders[i].getAbsolutePath();
 
-				}
-			}
-		}	
-		System.out.println("Git Path: " + gitPath);
-				
-		//get all java file names
-//		String [] fileNameList = null;
-//		findJavaClass(repoPath, fileNameList);
+                                }
+                        }
+                }        
+                System.out.println("Git Path: " + gitPath);
+                                
+                //get all java file names
+//                String [] fileNameList = null;
+//                findJavaClass(repoPath, fileNameList);
 
-		File currentDir = new File(System.getProperty("user.dir")).getParentFile();
-		localRepoUrl = currentDir + "/.git";
-		
-		LogGatherer lg = new LogGatherer();
-		Parser parser = new Parser();
-		
-		// pass the each class from the list of existing classes: in this case just passing the 'Class' class
-		// repeat this using loop for each passed class:
-		// 1.start gathering the log for the class
-		lg.startGatheringLog(localRepoUrl, "CS410 Project/src/main/Parser.java");
-		// 2.pass the gathered log information for the class to be parsed
-		parser.startParsingClass(lg);
-		// pass the class Object(s) (there could be nested classes, so it is Object(s)) parsed in the Parse class to the visualization class using  getParsedLog() in the Parse class
-		//...
+                File currentDir = new File(System.getProperty("user.dir")).getParentFile();
+                localRepoUrl = currentDir + "/.git";
+                
+                LogGatherer lg = new LogGatherer();
+                Parser parser = new Parser();
+                
+                // pass the each class from the list of existing classes: in this case just passing the 'Class' class
+                // repeat this using loop for each passed class:
+                // 1.start gathering the log for the class
+                lg.startGatheringLog(localRepoUrl, "CS410 Project/src/main/Parser.java");
+                // 2.pass the gathered log information for the class to be parsed
+                parser.startParsingClass(lg);
+                // pass the class Object(s) (there could be nested classes, so it is Object(s)) parsed in the Parse class to the visualization class using  getParsedLog() in the Parse class
+                //...
 
-		
-	/*	
-		File currentDir = new File(System.getProperty("user.dir")).getParentFile();
-		String gitDir = currentDir + "/.git";
-		//quinn test start
-		//LogGatherer lg = new LogGatherer(localRepoUrl, "CS410 Project/src/main/LogGatherer.java");
-		LogGatherer lg = new LogGatherer(gitDir, "CS410 Project/src/main/Main.java");
-		String[] strArray = lg.rawCode();
-		
-		//for(int i=0; i<lg.numLinesOfCode(); i++){
-		//	System.out.print(lg.getAuthor(i) + ": " + lg.rawCode(i) + " time: " + lg.getCommitTime(i));
-		//}
-		//quinn test end
+                
+        /*        
+                File currentDir = new File(System.getProperty("user.dir")).getParentFile();
+                String gitDir = currentDir + "/.git";
+                //quinn test start
+                //LogGatherer lg = new LogGatherer(localRepoUrl, "CS410 Project/src/main/LogGatherer.java");
+                LogGatherer lg = new LogGatherer(gitDir, "CS410 Project/src/main/Main.java");
+                String[] strArray = lg.rawCode();
+                
+                //for(int i=0; i<lg.numLinesOfCode(); i++){
+                //        System.out.print(lg.getAuthor(i) + ": " + lg.rawCode(i) + " time: " + lg.getCommitTime(i));
+                //}
+                //quinn test end
 */
-		LinkedList<Tower> towers = parser.getParsedLog();
-		RenderWindow window = new RenderWindow();
-		
-		VideoMode mode = new VideoMode(800, 600);
-		
-		window.create(mode, "Test");
-		
-		window.setFramerateLimit(60);
-		
-		CityModel city = new CityModel(window);
-		city.setTowers(towers);
-		CityController controller = new CityController(city);
-		
-		while (window.isOpen()) {
-			controller.updateModel();
-			city.drawCity();
-			window.display();
-		}
-	}
+                LinkedList<Tower> towers = parser.getParsedLog();
+                RenderWindow window = new RenderWindow();
+                
+                VideoMode mode = new VideoMode(800, 600);
+                
+                window.create(mode, "Test");
+                
+                window.setFramerateLimit(60);
+                
+                CityModel city = new CityModel(window);
+                city.setTowers(towers);
+                CityController controller = new CityController(city);
+                
+                while (window.isOpen()) {
+                        controller.updateModel();
+                        city.drawCity();
+                        window.display();
+                }
+        }
 
 }
