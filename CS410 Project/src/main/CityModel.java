58079754f1c6a8f1fae2297b3bcad4d502640a2a
yolandaw <yw.yunwing@gmail.com>
2013-11-18 16:08:25 -0800

    adding in tower signs, not complete

--- CS410 Project/src/main/CityModel.java
+++ CS410 Project/src/main/CityModel.java
@@ -51,6 +51,7 @@ public class CityModel {
 		
 		
 		int x = 400;
+		int x2;
 		towers = newTowers;
 		String currentPackage = newTowers.getFirst().getCityName();
 		
@@ -64,10 +65,11 @@ public class CityModel {
 			
 			//hack to see all towers
 			t.setTowerPosition(x, worldDimensions.height/2);	
+			x2=x;
 			x=x+t.getTowerWidth()+t.getTowerDepth();
 			//end hack to see all towers
 			
-			t.updateFloors(grassMidHeight);
+			t.updateFloors(grassMidHeight, x2);
 		}
 		
 		createGround();
