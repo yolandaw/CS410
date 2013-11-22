3c121dff79dbdc319ff124cbeb9fb171497bf18f
Chiho <blackho8291@gmail.com>
2013-11-22 13:08:13 +0900

    ignore this

--- CS410 Project/src/main/Parser.java
+++ CS410 Project/src/main/Parser.java
@@ -698,22 +698,19 @@ public class Parser {
 
         
            
-   private Author getUniqueAuthor(PersonIdent ownership) {
-	   
+   private Author getUniqueAuthor(PersonIdent tempOwnership) {
+	   PersonIdent ownership = tempOwnership;
        Author author = new Author("Empty", "Empty");
        String uniqueEmail = "UnknownEmailAddr";
        if(ownership != null) {
     	   uniqueEmail = ownership.getEmailAddress();
        }
-               
+
        if (!allAuthors.containsKey(uniqueEmail)) {
     	   String uniqueName = "UnknownName";
     	   if(ownership != null){
     		   uniqueName = ownership.getName();
-    	   }else {
-    		   System.out.println("NULL");
     	   }
-    	   
     	   author = new Author(uniqueName, uniqueEmail);
               
     	   if (uniqueEmail == "") {
